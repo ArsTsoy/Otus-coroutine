77fa49bd26355dbd546db9c531ba671b04061eda
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2020-04-10 21:21:31 +0600

    setup Fragment

--- app/src/main/java/kz/chocofamily/coroutinelesson/presentation/fragments/CryproCurrencyListFragment.kt
+++ app/src/main/java/kz/chocofamily/coroutinelesson/presentation/fragments/CryproCurrencyListFragment.kt
@@ -5,21 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kz.chocofamily.coroutinelesson.R
 
 /**
  * Created by Arslan Tsoy <t.me/arslantsoy> on 2020-04-10
  */
 
-class CryproCurrencyListFragment: Fragment() {
+class CryproCurrencyListFragment : Fragment() {
 
     //region Overridden Methods
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_list_currency, container, false)
+
 
     //endregion
 
