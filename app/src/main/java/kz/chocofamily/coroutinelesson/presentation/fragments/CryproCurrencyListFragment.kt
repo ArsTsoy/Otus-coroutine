06a5810475c95df3b5e489ec5150edfaeee4c44e
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2020-04-10 21:16:35 +0600

    add fragment

---
+++ app/src/main/java/kz/chocofamily/coroutinelesson/presentation/fragments/CryproCurrencyListFragment.kt
@@ -0,0 +1,27 @@
+package kz.chocofamily.coroutinelesson.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Arslan Tsoy <t.me/arslantsoy> on 2020-04-10
+ */
+
+class CryproCurrencyListFragment: Fragment() {
+
+    //region Overridden Methods
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    //endregion
+
+
+}
