4038ab3a78be5ef33865cfc83d03ef6629cf0343
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2020-05-28 16:34:37 +0600

    add pinCode navigation

--- app/src/main/java/kz/chocofamily/coroutinelesson/presentation/fragments/ProfileFragment.kt
+++ app/src/main/java/kz/chocofamily/coroutinelesson/presentation/fragments/ProfileFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 import kz.chocofamily.coroutinelesson.R
+import kz.chocofamily.coroutinelesson.presentation.MainActivity
 
 /**
  * Created by Arslan Tsoy <t.me/arslantsoy> on 2020-05-28
@@ -21,6 +23,10 @@ class ProfileFragment: Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnPinSettings.setOnClickListener {
+            (activity as MainActivity).navigateToPin()
+        }
+    }
 }
